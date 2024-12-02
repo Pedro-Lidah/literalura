7820af365b3e6f0960c6b4669d333ecaaa27ad64
Pedro-Lidah <pedro5369melo@gmail.com>
2024-12-02 15:40:21 -0300

    1.2

--- src/main/java/com/alura/literalura/principal/Main.java
+++ src/main/java/com/alura/literalura/principal/Main.java
@@ -1,8 +1,6 @@
 package com.alura.literalura.principal;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
 import java.io.IOException;
 import java.net.URI;
@@ -17,16 +15,28 @@ public class Main {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();        try {
 
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            
+
             if (response.statusCode() == 200) {
                 String responseBody = response.body();
-                Gson gson = new Gson();
-                
                 JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
-                System.out.println("Resposta da API: " + jsonObject);
-                
-             BookList books = gson.fromJson(responseBody, BookList.class);
-             books.results.forEach(book -> System.out.println("Título: " + book.title));
+
+                JsonArray results = jsonObject.getAsJsonArray("results");
+
+                for (JsonElement element : results) {
+                    JsonObject book = element.getAsJsonObject();
+                    String title = book.get("title").getAsString();
+
+                    System.out.println("Título: " + title);
+
+                    if (book.has("authors")) {
+                        JsonArray authors = book.getAsJsonArray("authors");
+                        for (JsonElement authorElement : authors) {
+                            JsonObject author = authorElement.getAsJsonObject();
+                            String authorName = author.get("name").getAsString();
+                            System.out.println("   Autor: " + authorName);
+                        }
+                    }
+                }
             } else {
                 System.out.println("Erro: " + response.statusCode());
             }
